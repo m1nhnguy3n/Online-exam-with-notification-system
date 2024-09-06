d2dc35371d40fe95aeb8d0dd420b3f40b82d75c0
m1nhnguy3n <minhdacamst@gmail.com>
2024-09-06 14:53:37 +0700

    feat(user-module) : Complete create user service

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,33 +1,54 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
-  UseInterceptors,
-  ParseUUIDPipe
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  UseInterceptors
 } from '@nestjs/common';
-import { UserService } from './user.service';
+import { UUID } from 'crypto';
+import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
-import { UUID } from 'crypto';
+import { UserService } from './user.service';
+import { ApiTags } from '@nestjs/swagger';
+import { CreateTeacherDto } from './dto/create-teacher.dto';
+import { CreateStudentDto } from './dto/create-student.dto';
 
 @Controller('users')
+@ApiTags('User')
 @UseInterceptors(LoggingInterceptor)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  async create(@Body() createUserDto: CreateUserDto) {
+  @Post('student')
+  async createStudent(@Body() createStudentDto: CreateStudentDto) {
     try {
-      await this.userService.create(createUserDto);
+      await this.userService.createStudent(createStudentDto, createStudentDto.parentNumber);
 
       return {
         success: true,
-        message: 'User Created Successfully'
+        message: 'Student Created Successfully'
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message
+      };
+    }
+  }
+
+  @Post('student')
+  async createTeacher(@Body() createTeacherDto: CreateTeacherDto) {
+    try {
+      await this.userService.createTeacher(createTeacherDto, createTeacherDto.subject);
+
+      return {
+        success: true,
+        message: 'Teacher Created Successfully'
       };
     } catch (error) {
       return {
