eacd090afb11662c39e82634f49f938bda668c55
HuyHue <dqh1005@gmail.com>
2024-09-06 16:24:16 +0700

    feat(question-module): done setup

--- src/modules/questions/dto/create-question.dto.ts
+++ src/modules/questions/dto/create-question.dto.ts
@@ -1,1 +1,18 @@
-export class CreateQuestionDto {}
+import { Expose } from "class-transformer";
+import { IsEnum, IsString } from "class-validator";
+import { QuestionType } from "src/entities/enums/question-type.enum";
+
+export class CreateQuestionDto {
+  @Expose()
+  @IsString()
+  @IsEnum(QuestionType)
+  type: string;
+
+  @Expose()
+  @IsString()
+  content: string;
+
+  @Expose()
+  @IsString()
+  categoryId: string;
+}
