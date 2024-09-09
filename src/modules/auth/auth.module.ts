1770786ca5181ff8493d0b2a6c125b5c3a4080c7
linhne94 <linhnqde160572@fpt.edu.vn>
2024-09-09 15:04:59 +0700

    fix after review

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { JwtModule } from '@nestjs/jwt';
   imports: [
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
-      secret: 'your_secret_key',
+      secret: process.env.JWT_SECRET_KEY,
       signOptions: { expiresIn: '15d' },
     }),
   ],
