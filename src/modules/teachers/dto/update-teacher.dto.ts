1db89c0ef71849cbdaae0aef785a1f62e0a00685
m1nhnguy3n <minhdacamst@gmail.com>
2024-09-13 08:20:34 +0700

    chore(teacher-module): format code

--- src/modules/teachers/dto/update-teacher.dto.ts
+++ src/modules/teachers/dto/update-teacher.dto.ts
@@ -1,6 +1,6 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional, IsString, MaxLength } from "class-validator";
-import { UpdateUserDto } from "src/modules/users/dto/update-user.dto";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
+import { UpdateUserDto } from 'src/modules/users/dto/update-user.dto';
 
 export class UpdateTeacherDto extends UpdateUserDto {
   @IsOptional()
